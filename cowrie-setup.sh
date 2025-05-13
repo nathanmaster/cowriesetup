f38dc54c8cd13d22551bc22529fb85e5cd91c041
gordondavis8379 <148791347+gordondavis8379@users.noreply.github.com>
2025-05-13 15:18:22 -0400

    Update cowrie-setup.sh

--- cowrie-setup.sh
+++ cowrie-setup.sh
@@ -1,61 +1,41 @@
 #!/bin/bash
 
-# Exit on error
 set -e
 
-# Update and install dependencies
-
 echo "[*] Installing dependencies..."
-sudo apt update && sudo apt install -y git python3-venv python3-pip libssl-dev libffi-dev build-essential libpython3-dev authbind
- 
-# Add cowrie user
+sudo apt update && sudo apt install -y git python3-venv python3-pip libssl-dev libffi-dev build-essential libpython3-dev authbind netfilter-persistent
+
 echo "[*] Adding Cowrie User..."
-
 sudo adduser --disabled-password --gecos "" cowrie
-sudo su - cowrie
- 
-# Clone Cowrie
+
 echo "[*] Cloning Cowrie..."
+sudo -u cowrie git clone https://github.com/cowrie/cowrie.git /home/cowrie/cowrie
+sudo -u cowrie rm -rf /home/cowrie/cowrie/.git
 
-git clone https://github.com/cowrie/cowrie.git
-cd cowrie
-rm -rf .git
- 
-# Setup virtualenv
-echo "[*] Setting up Virtual Enviroment..."
+echo "[*] Setting up Virtual Environment..."
+sudo -u cowrie python3 -m venv /home/cowrie/cowrie/cowrie-env
+sudo -u cowrie /home/cowrie/cowrie/cowrie-env/bin/pip install --upgrade pip
+sudo -u cowrie /home/cowrie/cowrie/cowrie-env/bin/pip install -r /home/cowrie/cowrie/requirements.txt
 
-python3 -m venv cowrie-env
-source cowrie-env/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
- 
-# Setup basic config
 echo "[*] Setting up Cowrie Configurations..."
+sudo -u cowrie cp /home/cowrie/cowrie/etc/cowrie.cfg.dist /home/cowrie/cowrie/etc/cowrie.cfg
+sudo -u cowrie cp /home/cowrie/cowrie/etc/userdb.txt.dist /home/cowrie/cowrie/etc/userdb.txt
 
-cp etc/cowrie.cfg.dist etc/cowrie.cfg
-cp etc/userdb.txt.dist etc/userdb.txt
- 
-
-# Install obscurer.py
 echo "[*] Installing obscurer..."
-
-cd ~
 git clone https://github.com/411Hall/obscurer.git
 cd obscurer
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+python3 obscurer.py --cowrie_path /home/cowrie/cowrie --os ubuntu --version 22.04
+cd ..
+rm -rf obscurer
 
-# Apply Obscurer to Cowrie install
-python3 obscurer.py --cowrie_path ~/cowrie --os ubuntu --version 22.04
-
-# Redirect port 22 to Cowrie
 echo "[*] Redirecting port 22 to 2222..."
 sudo iptables -t nat -A PREROUTING -p tcp --dport 22 -j REDIRECT --to-port 2222
 sudo netfilter-persistent save
 
-#Start cowrie
 echo "[*] Starting Cowrie..."
-sudo -u cowrie bash -c "cd ~/cowrie && source cowrie-env/bin/activate && authbind --deep ./home/cowrie/cowrie/bin/cowrie start"
+sudo -u cowrie bash -c "cd /home/cowrie/cowrie && source cowrie-env/bin/activate && authbind --deep ./bin/cowrie start"
 
 echo "[✓] Obscured Cowrie honeypot is live!"
